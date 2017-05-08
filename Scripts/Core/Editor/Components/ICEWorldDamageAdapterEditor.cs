a9394bcc458aa1c579e682a171d9c04229a5f65c
icetec <pit@ice-technologies.de>
2017-05-08 02:06:55 +0200

    Add INVECTOR and Ultimate Survival integration

--- Scripts/Core/Editor/Components/ICEWorldDamageAdapterEditor.cs
+++ Scripts/Core/Editor/Components/ICEWorldDamageAdapterEditor.cs
@@ -38,6 +38,10 @@ namespace ICE.Integration.Adapter
 
 		public void DrawAdapterContent( ICEWorldDamageAdapter _adapter )
 		{
+			EditorGUILayout.HelpBox( "The ICEWorldDamageAdapter handles the damage impacts of " +
+				"supported 3rd Party Assets. Add this script to all ICE entities that can be " +
+				"damaged (e.g. creatures and their body parts, objects, items etc.).", MessageType.None );
+
 			#if ICE_OPSIVE_TPC
 			#elif ICE_OPSIVE_TPC
 			#elif ICE_UFPS
@@ -46,7 +50,7 @@ namespace ICE.Integration.Adapter
 			#elif ICE_UNITZ
 			#elif ICE_EASY_WEAPONS
 			#elif ICE_ULTIMATE_SURVIVAL
-
+			/*
 			UltimateSurvival.EntityEventHandler _entity_event_handler = _adapter.gameObject.GetComponentInParent< UltimateSurvival.EntityEventHandler >();
 
 			EditorGUI.BeginDisabledGroup( _entity_event_handler != null );
@@ -54,7 +58,7 @@ namespace ICE.Integration.Adapter
 			if( ICEEditorLayout.Button( "Entity Event Handler", "", ICEEditorStyle.ButtonExtraLarge ) )
 				_entity_event_handler = _adapter.gameObject.AddComponent<UltimateSurvival.EntityEventHandler>();
 			GUI.backgroundColor = ICEEditorLayout.DefaultBackgroundColor;
-			EditorGUI.EndDisabledGroup();
+			EditorGUI.EndDisabledGroup();*/
 			#elif ICE_UMMORPG
 			#else
 			#endif
